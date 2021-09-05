65f33f909011deac5e2067befb29c12b62b13a9d
Ian Schoenrock <32747586+theapphideaway@users.noreply.github.com>
2021-09-05 13:06:01 -0400

    Added more to the Notes list

--- app/src/main/java/com/ianschoenrock/wfhcompanion/models/Note.kt
+++ app/src/main/java/com/ianschoenrock/wfhcompanion/models/Note.kt
@@ -2,6 +2,7 @@ package com.ianschoenrock.wfhcompanion.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "note_table")
 class Note(
--- app/src/main/java/com/ianschoenrock/wfhcompanion/screens/NotesScreen.kt
+++ app/src/main/java/com/ianschoenrock/wfhcompanion/screens/NotesScreen.kt
@@ -2,6 +2,7 @@ package com.ianschoenrock.wfhcompanion.screens
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,54 +24,113 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun NotesScreen(){
     val notes = remember{mutableStateOf(ArrayList<String>())}
-    notes.value.addAll(listOf("One", "Two", "Three"))
+    if(notes.value.size < 1){
+        notes.value.addAll(listOf("One", "Two", "Three"))
+    }
+    var selectedNote = remember {mutableStateOf("")}
     if(LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE){
         Row(verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()){
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .weight(0.5f)) {
+                Card(Modifier.padding(16.dp),
+                    shape = RoundedCornerShape(20.dp)
+                ) {
+                    Text(
+                        "New Note ",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
+                    )
+
+                }
+                LazyColumn(Modifier.padding(bottom = 50.dp)) {
+                    items(notes.value.count()) { index ->
+                        Card(Modifier.padding(16.dp),
+                            shape = RoundedCornerShape(20.dp)
+                        ) {
+                            Text(
+                                notes.value[index],
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                                    .clickable {
+                                        selectedNote.value = notes.value[index]
+                                    },
+                                style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
+                            )
+
+                        }
+                    }
+
+                }
+            }
             Box(
                 Modifier
                     .fillMaxSize()
-                    .background(Color.Green)
-                    .weight(0.5f)) {}
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .background(Color.Blue)
-                    .weight(1f)) {}
+                    .weight(1f)){
+                DetailsScreen(selectedNote.value)
+            }
         }
     } else{
-        Column(verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()) {
-            Card(Modifier.padding(16.dp),
-                shape = RoundedCornerShape(20.dp)
+        if(selectedNote.value == "") {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
             ) {
-                Text(
-                    "New Note ",
-                    modifier = Modifier.fillMaxWidth()
-                        .padding(16.dp),
-                    style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
-                )
+                Card(
+                    Modifier.padding(16.dp),
+                    shape = RoundedCornerShape(20.dp)
+                ) {
+                    Text(
+                        "New Note ",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
+                    )
 
-            }
-            LazyColumn(Modifier.padding(bottom = 50.dp)) {
-                items(notes.value.count()) { index ->
-                    Card(Modifier.padding(16.dp),
-                        shape = RoundedCornerShape(20.dp)
-                    ) {
-                        Text(
-                            notes.value[index],
-                            modifier = Modifier.fillMaxWidth()
-                                .padding(16.dp),
-                            style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
-                        )
-
-                    }
                 }
+                LazyColumn(Modifier.padding(bottom = 50.dp)) {
+                    items(notes.value.count()) { index ->
+                        Card(
+                            Modifier.padding(16.dp),
+                            shape = RoundedCornerShape(20.dp)
+                        ) {
+                            Text(
+                                notes.value[index],
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                                    .clickable {
+                                        selectedNote.value = notes.value[index]
+                                    },
+                                style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
+                            )
 
+                        }
+                    }
+
+                }
             }
+        }
+        else{
+            DetailsScreen(content = selectedNote.value)
         }
     }
 
+}
+
+@Composable 
+fun DetailsScreen(content: String){
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()) {
+        Text("Details: $content")
+    }
 }
