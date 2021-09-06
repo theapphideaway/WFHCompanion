d75bad45a2722013c8b1dbf92914659742313f9d
Ian Schoenrock <32747586+theapphideaway@users.noreply.github.com>
2021-09-06 10:43:16 -0400

    Formatted the note UI

--- app/src/main/java/com/ianschoenrock/wfhcompanion/screens/NotesScreen.kt
+++ app/src/main/java/com/ianschoenrock/wfhcompanion/screens/NotesScreen.kt
@@ -2,23 +2,29 @@ package com.ianschoenrock.wfhcompanion.screens
 
 import android.content.res.Configuration
 import android.util.Log
+import android.widget.EditText
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,14 +44,17 @@ fun NotesScreen(){
                 Modifier
                     .fillMaxSize()
                     .weight(0.5f)) {
-                Card(Modifier.padding(16.dp),
+                Card(Modifier.padding(16.dp) .clickable {
+                    selectedNote.value = "New"
+                },
                     shape = RoundedCornerShape(20.dp)
                 ) {
                     Text(
                         "New Note ",
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(16.dp),
+                            .padding(16.dp)
+                           ,
                         style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center)
                     )
 
@@ -86,7 +95,9 @@ fun NotesScreen(){
                 modifier = Modifier.fillMaxSize()
             ) {
                 Card(
-                    Modifier.padding(16.dp),
+                    Modifier.padding(16.dp).clickable {
+                        selectedNote.value = "New"
+                    },
                     shape = RoundedCornerShape(20.dp)
                 ) {
                     Text(
@@ -133,10 +144,50 @@ fun NotesScreen(){
 
 @Composable 
 fun DetailsScreen(content: String){
-
-    Column(verticalArrangement = Arrangement.Center,
+    val focusManager = LocalFocusManager.current
+    val titleState = remember { mutableStateOf("") }
+    val contentState = remember { mutableStateOf("") }
+    if(content != "New"){
+        titleState.value = content
+    }
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()) {
-        Text("Details: $content")
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = titleState.value,
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Words,
+                    imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { focusManager.clearFocus(true)}
+                ),
+                onValueChange = { titleState.value = it },
+                placeholder = {Text(text = "Enter New Title", fontSize = 24.sp)},
+                textStyle = TextStyle(fontSize = 24.sp),
+                colors = TextFieldDefaults.textFieldColors(
+                    disabledTextColor = Color.Transparent,
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                ),
+            )
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = contentState.value,
+                onValueChange = { contentState.value = it },
+                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+                placeholder = {Text(text = "Enter New Note", fontSize = 18.sp)},
+                textStyle = TextStyle(fontSize = 18.sp),
+                colors = TextFieldDefaults.textFieldColors(
+                    disabledTextColor = Color.Transparent,
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
+            )
+
     }
 }
